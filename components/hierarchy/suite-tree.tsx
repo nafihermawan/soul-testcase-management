2c3e9a5de710997c3e5f83999778e2926c9547be
Nafi Hermawan <nafihermawan@gmail.com>
2026-09-10 11:23:54 +0700

    Update column header from "Kode" to "id Suite" for clarity in SuiteTree component

--- components/hierarchy/suite-tree.tsx
+++ components/hierarchy/suite-tree.tsx
@@ -398,7 +398,7 @@ export function SuiteTree({
                   }}
                 >
                   <th style={{ padding: "0.6rem 1.25rem", fontWeight: 600 }}>Nama Suite</th>
-                  <th style={{ padding: "0.6rem 0.5rem", fontWeight: 600 }}>Kode</th>
+                  <th style={{ padding: "0.6rem 0.5rem", fontWeight: 600 }}>id Suite</th>
                   <th style={{ padding: "0.6rem 0.5rem", fontWeight: 600 }}>Jumlah Test Case</th>
                   <th style={{ padding: "0.6rem 0.5rem", fontWeight: 500, color: "#6B7280" }}>
                     Last Updated
